484fcb21751b59b9a13530c125bdeb95af970de4
Darren James <darrenjames.dalino@gmail.com>
2020-08-27 15:17:05 +0800

    updated

--- client-side/src/components/Step/StepTwoB.jsx
+++ client-side/src/components/Step/StepTwoB.jsx
@@ -52,8 +52,8 @@ const StepTwo = () => {
         data="Take a Selfie with the 2 Valid IDs one by one"
       />
 
-      <div className="border position-relative mx-auto box-h250 box-w300">
-        <div className="position-absolute border-inner-box box-h260 box-w225"></div>
+      <div className="border position-relative mx-auto box-h300 box-w300">
+        <div className="position-absolute border-inner-box box-h310 box-w225"></div>
         <div className="position-absolute box-img w-90 h-100 py-10">
           {stepTwoUploadB.length < 1 ? (
             <img
